59ecfd97768dc69cc80bda515ff7fea82986ee84
Аkril0 <alexbatin2003@gmail.com>
2024-06-16 23:25:25 +0300

    Add list

--- src/components/HeroList.tsx
+++ src/components/HeroList.tsx
@@ -2,50 +2,57 @@
 
 import {useCallback, useEffect, useState} from "react";
 import {fetchHeroes} from "@/services/api";
-
+import { useTransition, animated } from '@react-spring/web'
 
 const HeroList: React.FC = () => {
     const [heroes, setHeroes] = useState<any[]>([]);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
 
+
     useEffect(() => {
-        const loadFunc = async () => {
+        (async () => {
             setLoading(true);
             const newHeroes = await fetchHeroes(page);
             console.log(newHeroes);
             setHeroes(prev => [...prev, ...newHeroes.results]);
             setLoading(false);
-        }
-        loadFunc();
+        })();
     }, [page]);
 
-    const handleScroll = useCallback(() => {
-        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) {
-            return;
-        }
-        setPage(prev => prev + 1);
-    }, [loading]);
+    const [transitions, api] = useTransition(page, () => ({
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 1 },
+    }))
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, [handleScroll]);
     const handleButton = () => {
         setPage(prev => prev + 1);
     }
     return (
         <div className="p-4">
-            <ul>
+            <ul className="p-4 grid gap-4 md:grid-cols-5 grid-cols-2">
                 {heroes.map(hero => (
                     <li key={hero.id}
-                        className="cursor-pointer p-2 border-b">
+                        className="
+                            h-28
+                            cursor-pointer
+                            bg-blue-600
+                            rounded
+                            p-2
+                            hover:bg-blue-700
+                            transition">
                         {hero.name}
                     </li>
                 ))}
             </ul>
-            <button onClick={handleButton}>Ещё</button>
-            {loading && <div className="loader"></div>}
+            {!loading && <button className="mx-auto block"
+                                 onClick={handleButton}>
+                Ещё
+            </button>}
+            {loading && <div className="loader">
+
+            </div>}
         </div>
     );
 };
